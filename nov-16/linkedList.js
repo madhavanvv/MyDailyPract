0ed4ed48ab90f49662fe8ffcf17807fa4c8dc5b8
madhavanvv <madhavan1177@gmail.com>
2022-11-16 11:04:53 +0530

    readme fiel added

--- nov-16/linkedList.js
+++ nov-16/linkedList.js
@@ -1,3 +1,5 @@
+//Date:16-11-2022
+//concept:LinkedList
 class Node {
   constructor(data) {
     this.data = data;
@@ -17,6 +19,14 @@ class LinkedList {
   llDisplay() {
     console.log(this.head);
   }
+  removeAtHead() {
+    if (this.head == null) {
+      return;
+    }
+    let tmp = this.head.next;
+    this.head.next = null;
+    this.head = tmp;
+  }
 }
 
 let ll1 = new LinkedList();
@@ -24,4 +34,6 @@ ll1.addAtHead(5);
 ll1.addAtHead(4);
 ll1.addAtHead(3);
 ll1.addAtHead(2);
+ll1.llDisplay();
+ll1.removeAtHead();
 ll1.llDisplay();
